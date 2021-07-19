7c1a755b9129c93b20d13cd228b05517e0b1a373
muzammil-aslam <muzammilaslam914@gmail.com>
2021-07-19 14:46:28 -0700

    first css ki style ki hat

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-function getValue(value){
-    var box=document.getElementById('box');
-    box.value=box.value+value
+function getValue(value) {
+  var box = document.getElementById("box");
+  box.value = box.value + value;
 }
 
-function calculate(){
-    var box=document.getElementById('box');
-    var total=eval(box.value)
-    box.value=total
+function calculate() {
+  var box = document.getElementById("box");
+  var total = eval(box.value);
+  box.value = total;
 }
